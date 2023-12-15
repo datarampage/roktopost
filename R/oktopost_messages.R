814763efcfb2ac3d6d128a25be965b275372f485
Randall Helms <124309699+datarampage@users.noreply.github.com>
2023-12-15 13:09:03 +0100

    DIT-947 message function

---
+++ R/oktopost_messages.R
@@ -0,0 +1,38 @@
+#' oktopost_messages
+#'
+#' Get information about an individual message
+#' @param message_id The post ID
+#' @export
+#'
+
+oktopost_messages <- function(message_id) {
+
+  require(httr)
+  require(purrr)
+  require(tidyverse)
+
+  base_url = "https://api.oktopost.com/v2"
+  endpoint <- "message"
+
+  url <- paste(paste(base_url,endpoint,message_id,sep='/'),'withTags=1',sep='?')
+
+  account_id <- Sys.getenv('OKTOPOST_ACCOUNT_ID')
+  access_token <- Sys.getenv('OKTOPOST_ACCESS_TOKEN')
+
+  result <- GET(url,authenticate(account_id,access_token))
+
+  result2 <- content(result,'parsed')
+
+  message <- result2$Message
+
+  message$Media <- NULL
+  message$AltTexts <- NULL
+  message$Tags <- NULL
+
+  message <- as_tibble(nullToNA(message))
+
+  names(message) <-  to_snake_case(names(message))
+
+  return(message)
+
+}
